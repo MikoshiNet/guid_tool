1cff66b51b59b29fb05196b5ccb1e4896b67e25d
Antglo <jtab1277@gmail.com>
2024-04-11 23:26:39 -0400

    v0.1

--- modules/db.py
+++ modules/db.py
@@ -1,84 +1,38 @@
 # pylint: disable=missing-docstring, import-error
-'''Pevious code (Using as guidlines)'''
-###from modules.file_handler import get_file_json_content, set_file_json_content
-###
-###class Database:
-###    def __init__(self, path):
-###        self.path = path
-###        self._get()
-###
-###    def _get(self):
-###        self.database = get_file_json_content(self.path)
-###
-###    def _set(self):
-###        set_file_json_content(self.path, self.database)
-###
-###    def post_id(self, device_type, description):
-###        new_id = self.database['devices'][len(self.database['devices'])][1] + 1
-###        self.database['devices'].append(device_type, new_id, description, "")
-###
-###        return f"{device_type}{new_id}"
-
-# modules/db.py
-# Antglo 3/4/2024
-### This will have examples on how to manipulate items within the DB using the Session
-
-from models import engine, Devices
-<<<<<<< HEAD
-from sqlalchemy import select
-from sqlalchemy.orm import sessionmaker
-
-# Create a sessionmaker bound to the engine
-Session = sessionmaker(bind=engine)
-# create a session
-session = Session()
-
-# Add a device to the database [ device, name, desc ]
-def add_device():
-=======
+from models import Devices
+from flask_restful import Resource, reqparse
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-Session = sessionmaker
+# Create the engine
+engine = create_engine('sqlite:///modules/database.db', echo=True)
 
-Session.configure(bind=engine)
+# Initiate sessionmake to bind to the engine 
+Session = sessionmaker(bind=engine)
 
-#add a device to the database [ device, name, desc ]
-def add_device(device_name, device_desc):
->>>>>>> 395fa0f (patch)
-    '''This is an example on how it is done'''
-    with Session() as session:
-        sca = Devices(
-            device='sca',
-            name='Security Appliance',
-            desc='This is a security appliance'
-        )
-    
-        session.add(sca)
-        session.commit()
+# Resources for devices
+class DeviceResouce(Resource):
+    def get(self):
+        '''Query device table'''
+        session = Session()
 
-<<<<<<< HEAD
-add_device()
+        devices = session.query(Devices).all()
+        session.close()
+        return{'devices': [{'id':device.id, 'device':device.device, 
+                            'name':device.name, 'desc':device.desc
+                            } for device in devices]}, 200
+    def post(self):
+        # Parse the request data
+        parser = reqparse.RequestParser()
+        parser.add_argument('device', type=str, required=True)
+        parser.add_argument('name', type=str, required=True)        
+        parser.add_argument('desc', type=str, required=False)
+        args = parser.parse_args()        
 
-# Using select statement to query database.db
-stmt = session.query(Devices).filter(Devices.id == "1").all() # .one() can be used for a specific query
-print(stmt)
-=======
-def query_device():
-    '''Query device table'''
-    session = Session()
+        # insert a device into the database
+        with Session() as session:
+            new_device = Devices(device=args['device'], name=args['name'], desc=args['desc'])
+            session.add(new_device)
+            session.commit()
 
-    devices = session.query(Devices).all()
-
-    session.close()
-
-    return devices
-
-if __name__ == '__main__':
-    add_device('exmaple', 'example desc for the example')
-
-    devices = query_device()
-    print('Example: ')
-    for device in devices:
-        print(device)
->>>>>>> 395fa0f (patch)
+        return {'message': 'Device added bitch!'}, 201
